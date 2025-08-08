2961073af1d355b6b97c1843baf4e959e0bd059c
yassinemchairia <115500157+yassinemchairia@users.noreply.github.com>
2025-08-08 10:27:01 +0100

    Update alert.service.ts

--- src/app/Service/alert.service.ts
+++ src/app/Service/alert.service.ts
@@ -32,8 +32,7 @@ export class AlertService {
       emplacement: alerte.emplacement,
       description: alerte.description || '',
     };
-    return this.http.post<{ solution: string; similar_alertes: any[] }>(this.flaskApiUrl, payload);
-  }
+return this.http.post<{ solution: string; similar_alertes: any[] }>(`${this.apiUrl}/predict-solution`, payload);  }
 
   createIntervention(idAlerte: number): Observable<any> {
     return this.http.post(`${this.apiUrl}/predict-and-create-intervention/${idAlerte}`, {});
